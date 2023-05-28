7a542565fead3d41f0aa41bd6d303152bd564569
purvigangrade <purvigangrade6@gmail.com>
2023-05-28 10:56:57 +0530

    Assignment added

---
+++ tictactoe-using-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Box from './components/Box';
+
+function App() {
+  return (
+    <div className="App">
+      <Box />
+    </div>
+  );
+}
+
+export default App;
